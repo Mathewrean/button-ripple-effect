dd19b420c7b87fcbc7f5d23f9ab8699a9cdcbc1b
mathewrean <mathewrean@kali>
2024-11-30 18:09:25 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const btn1 = document.querySelector('.btn');
+
+btn1.addEventListener('mouseover', (event)=>{
+   const y = event.pageY - btn1.offsetTop;
+   const x = event.pageX - btn1.offsetLeft;
+
+   btn1.style.setProperty('--xPos', x + "px");
+   btn1.style.setProperty('--yPos', y + "px");
+});
